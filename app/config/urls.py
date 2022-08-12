8805959624a4a387cc2c073c01450a214e99281b
ovsds <ovsds@yandex-team.ru>
2022-08-13 01:36:08 +0300

    clean repo

--- app/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('debug/', include('debug_toolbar.urls')),
-    path('api/', include('movies.api.urls')),
-
-]
--- app/movies/base/classes.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-class Pagination(PageNumberPagination):
-    page_size = 50
-    page_query_param = 'page_size'
